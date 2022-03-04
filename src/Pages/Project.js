3b7d93460420f85ed47735f393064e98fb48fd6c
Meghan La Breche <Megavictoria@gmail.com>
2022-03-03 20:35:13 -0800

    build march 3

--- src/Pages/Project.js
+++ src/Pages/Project.js
@@ -4,15 +4,17 @@ import projects from '../shared/projects';
 
 function Project() {
 	return (
-		<div className='container'>
+		<div className='container wrapper'>
 			<div className='row'>
-				<div className='col md-4'></div>
-				{projects.map((project) => (
-					<div key={project.id}>
-						<h3>{project.title}</h3>
-						<p>{project.description}</p>
-					</div>
-				))}
+				<div className='col md-4'>
+					{projects.map((project) => (
+						<div key={project.id} className='project_card'>
+							<h3 className='card_title'>{project.title}</h3>
+							<p className='card_description'>{project.description}</p>
+							<button className='card_button'>View Project</button>
+						</div>
+					))}
+				</div>
 			</div>
 		</div>
 	);
